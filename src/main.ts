ece5924f7e93ffbdf70baf33b62680706a842b4a
Roman <technopriest666@gmail.com>
2023-07-10 17:06:43 +0300

    export enum && version++

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,23 @@ export { stopRecording, type ISound, type IStopRecordingOptions } from "./stop-r
 export { VoiceRecorder } from "./components/voice-recorder";
 export { downloadRecords } from "./download-records";
 export { useContainer } from "./hooks/use-container";
+export { usePermissions } from "./hooks/use-permissions";
 export { useRecordingCycle, type IUseRecordingCycle } from "./hooks/use-recording-cycle";
 export { useRecordingStatus } from "./hooks/use-recording-status";
 export { uploadRecords, type IRecord, type IUploadRecordsOptions } from "./upload-records";
+export { LinkName } from './link-name';
 
 import { startRecording } from './strart-recording';
 import { pauseRecording } from "./pause-recording";
 import { resumeRecording } from "./resume-recording";
 import { stopRecording } from "./stop-recording";
+import { uploadRecords } from "./upload-records";
 
 const Recorder = {
     startRecording,
     pauseRecording,
     resumeRecording,
-    stopRecording
+    stopRecording,
+    uploadRecords
 };
 export default Recorder;
