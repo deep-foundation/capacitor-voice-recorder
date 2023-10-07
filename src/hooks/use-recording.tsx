6e90bc97c0a45c8aa609567b46f1eb90a8b6262b
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-07 14:03:05 +0600

    Save every minute by defualt, add logs

--- src/hooks/use-recording.tsx
+++ src/hooks/use-recording.tsx
@@ -5,28 +5,36 @@ import {
   IStopAndUploadRecordingOptions,
   stopAndUploadRecording,
 } from "../stop-and-upload-recording.js";
+import { packageLog } from "../package-log.js";
 
 export function useRecording(options: IUseRecordingOptions) {
+  const log = packageLog.extend(useRecording.name)
   // State to capture any potential errors
   const [error, setError] = useState<unknown | null>(null);
+  log({error, setError})
 
   // Ref to store the timeout ID, ensuring proper cleanup on unmount
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  log({timeoutRef})
 
   const initiateRecording = async () => {
     try {
       await startRecording();
+      log("Recording started")
       const { savingIntervalInMs = 1*60*1000 } = options;
 
       timeoutRef.current = setTimeout(() => {
+        log("Going to stop and upload recording")
         stopAndUploadRecording(options).catch(setError);
       }, savingIntervalInMs);
-    } catch (err) {
-      setError(err);
+    } catch (error) {
+      log({error: error})
+      setError(error);
     }
   };
 
   const cleanupRecording = () => {
+    log("Cleaning up recording")
     stopAndUploadRecording(options);
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
---
+++ src/package-log.ts
@@ -0,0 +1,3 @@
+import debug from "debug";
+
+export const packageLog = debug('@deep-foundation/capacitor-voice-recorder')
