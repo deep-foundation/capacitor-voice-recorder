0ec9edfffc3d7e93af0cb26bd4a63633984bfcc5
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-07 23:25:53 +0600

    Make containerLinkId optional

--- src/stop-and-upload-recording.tsx
+++ src/stop-and-upload-recording.tsx
@@ -3,12 +3,12 @@ import { stopRecording } from "./stop-recording.js";
 import { uploadRecords } from "./upload-records.js";
 
 export async function stopAndUploadRecording(options: IStopAndUploadRecordingOptions) {
-  const { deep,  containerLinkId } = options;
+  const { deep,  containerLinkId = deep.linkId! } = options;
   const {endTime,startTime, sound} = await stopRecording();
   await uploadRecords({ deep, containerLinkId, records: [{ sound, startTime: startTime, endTime: endTime }] });
 }
 
 export type IStopAndUploadRecordingOptions = {
   deep: DeepClient;
-  containerLinkId: number;
+  containerLinkId?: number;
 };
